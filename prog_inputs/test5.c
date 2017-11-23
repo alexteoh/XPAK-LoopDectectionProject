45b05199039195381dc8972d1334634f45d33cc8
Plato Leung <platoleunghk@yahoo.com.hk>
2017-11-22 23:32:14 -0500

    test5.c add for chickin5 to check nested_loop dependences

---
+++ prog_inputs/test5.c
@@ -0,0 +1,16 @@
+int mss(int *a, int n){
+    int mss = 0;
+    int sum = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++) {
+			a[i+1][j-2] = a[i][j]+a[i-1][j];
+        }
+    }
+}
+
+int main(int argc, char** argv){
+    int n = 1000;
+    int* a;
+    a = malloc(n * sizeof(a));
+    return mss(a, n);
+}
