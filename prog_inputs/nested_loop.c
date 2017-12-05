e19f4a8fbe1cfefb7717582448603c809a637169
Alex <ateoh.dev@gmail.com>
2017-12-05 16:02:54 -0500

    Added second nested loops

--- prog_inputs/nested_loop.c
+++ prog_inputs/nested_loop.c
@@ -2,10 +2,18 @@ int mss(int *a, int n){
     int mss = 0;
     int mts = 0;
     int sum = 0;
+    int rand[n];
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++) {
             for(int k = 0; k < n; k++) {
                 sum += a[i][j][k];
+            }
+        }
+    }
+    for(int i2 = 0; i2 < n; i2++){
+        for(int j2 = 0; j2 < n; j2++) {
+            for(int k2 = 0; k2 < n; k2++) {
+                rand[i2] = a[j2] + a[k2];
             }
         }
     }
